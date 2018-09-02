f2660e225da9ee91bd48f9c67b82965a37ea48c8
Su Tasbas <tasbas.su@gmail.com>
2018-09-02 14:51:22 +0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello World!</h1>
+    <p>I'm Su.</p>
+    <p>Excited to join JAMstack Hackaton '18</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
